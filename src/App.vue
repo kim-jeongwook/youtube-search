a77aaf9d555cbd49104fca16ccb90cba43bab241
WMW <mybiggold@naver.com>
2020-06-16 15:01:34 +0900

    유튭

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <h1>욱튜브</h1>
+    <SearchBar @textInput="input" v-bind:results="results" />
+    <div id="app2">
+      <VideoPlayer v-bind:results="results" />
+      <VideoList v-bind:results="results" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar.vue";
+import VideoList from "./components/VideoList.vue";
+import VideoPlayer from "./components/VideoPlayer.vue";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    SearchBar,
+    VideoList,
+    VideoPlayer
+  },
+  data() {
+    return {
+      results: []
+    };
+  },
+  methods: {
+    input(userInput) {
+      // 1. 입력된 검색어를 가지고,
+      const baseUrl = "https://www.googleapis.com/youtube/v3/search";
+      const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+
+      // 2. Youtube API에 요청을 보내어
+      axios
+        .get(baseUrl, {
+          params: {
+            //key, part, q
+            key: API_KEY,
+            part: "snippet",
+            type: "video",
+            q: userInput,
+            maxResults: 10
+          }
+        })
+        // 3. 검색어로 검색한 결과를 가져옴
+        .then(response => {
+          console.log(userInput);
+          console.log(response.data.items);
+          this.results = response.data.items;
+        })
+        .catch();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#app2 {
+  display: flex;
+}
+</style>
---
+++ src/components/VideoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="result in results" v-bind:key="result.id.videoId">
+        {{ result.snippet.title }}
+        <img v-bind:src="result.snippet.thumbnails.default.url" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "VideoItem",
+  data() {
+    return {
+      userInput: "",
+      results: [],
+    };
+  },
+  methods: {
+    input() {
+      // 1. 입력된 검색어를 가지고,
+      const baseURL = "https://www.googleapis.com/youtube/v3/search";
+      const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+      // 2. Youtube API에 요청을 보내어
+      axios
+        .get(baseURL, {
+          params: {
+            // key, part, q
+            key: API_KEY,
+            part: "snippet",
+            type: "video",
+            q: this.userInput,
+            maxResults: 10,
+          },
+        })
+        .then((response) => {
+          console.log(response.data.items);
+          this.results = response.data.items;
+        });
+      // 3. 검색어로 검색한 결과를 가져옴
+      console.log("검색어 입력 되었음");
+    },
+  },
+};
+</script>
+
+<style></style>
